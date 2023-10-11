24c1ec30e65564fca92d69a4aea3cf73dd542483
poojagithub <A00302457@mycambian.ca>
2023-10-10 23:29:59 -0600

    my program

--- MultiCalculator/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ MultiCalculator/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.multicalculator.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +17,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.max
@@ -41,7 +43,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CalcView(){
    val displayText = remember { mutableStateOf("0")}
-    Column {
+    Column(modifier = Modifier.background(Color.LightGray)){
 
     }
 
